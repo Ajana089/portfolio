e077f7c1240fc20ac42499e6c9ed688a7d9334d8
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-10-20 19:04:06 +0400

    m

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portfolio'));
+app.use(express.static('./dist/Portfolio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portfolio/'}),
+    res.sendFile('index.html', {root: 'dist/Portfolio/'}),
 );
 
 // Start the app by listening on the default Heroku port
